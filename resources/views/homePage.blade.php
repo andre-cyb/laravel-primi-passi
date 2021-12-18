17fa6f33fb9db65b2053b1c5edff9b16a1d48cab
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-12-18 15:36:08 +0100

    laravel stampa dei dati passati tramite view()

---
+++ resources/views/homePage.blade.php
@@ -0,0 +1,28 @@
+@php
+$titoloBenvenuto = "Benvenuto nella Home Page";
+
+@endphp
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+
+    <h1>{{ $titoloBenvenuto }}</h1>
+    <h2>esempio di dati passati tramite view()</h2>
+
+    <ul>
+        <li>{{ $brand }}</li>
+        <li>{{ $model }}</li>
+        <li>{{ $km }} km</li>
+        <li>{{ $color }}</li>
+    </ul>
+
+</body>
+</html>
+
